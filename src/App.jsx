10c543c518a126093d83ff939fa9f013bbc1b1d0
Joe Aguado <aguado.joe@gmail.com>
2024-02-05 09:27:21 -0600

    added: nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import React from "react"
 import styles from './styles'
+
+import {
+  Nav,
+} from "./components"
 
 
 const App = () => (
-<div>
-  Hello World
+<div className="bg-primary w-full overflow-hidden">
+
+  <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Nav />
+    </div>
+  </div>
+
+
 </div>
 
 )
