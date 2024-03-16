8f7465947b0e719236d5905cf3dbcbd19e6ad020
Joe Aguado <aguado.joe@gmail.com>
2024-03-16 15:10:09 -0500

    removed: deployment files --J

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/zportfolio/",
+  // build: {
+  //   rollupOptions: {
+  //     input: {cname: "CNAME" } 
+  //   }
+  // },
   plugins: [react()],
 })
